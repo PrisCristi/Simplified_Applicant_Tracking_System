7aabd0f473b787d6b8ad72c85dba6c0226ff8561
PrisCristi <santos.priscila85@gmail.com>
2023-11-07 14:51:41 +0100

    Methods to Recruter class created

--- src/main/java/org/example/JobPosition.java
+++ src/main/java/org/example/JobPosition.java
@@ -12,13 +12,15 @@ public class JobPosition {
     String industry;
     String role;
 
-    public JobPosition(String title, String description, double offeredSalaryRangeStart, double offeredSalaryRangeEnd, List<String> requiredSkills, String location) {
+    public JobPosition(String title, String description, double offeredSalaryRangeStart, double offeredSalaryRangeEnd, List<String> requiredSkills, String location, String industry, String role) {
         this.title = title;
         this.description = description;
         this.offeredSalaryRangeStart = offeredSalaryRangeStart;
         this.offeredSalaryRangeEnd = offeredSalaryRangeEnd;
         this.requiredSkills = requiredSkills;
         this.location = location;
+        this.industry = industry;
+        this.role = role;
     }
     public boolean isWithinBudget(Applicant applicant){ // etiqueta
         double withinBudget = applicant .getExpectedSalary(); // declarando withinBuget com o valor expectativa salarial
@@ -75,5 +77,21 @@ public class JobPosition {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
--- src/main/java/org/example/Recruter.java
+++ src/main/java/org/example/Recruter.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -8,36 +9,65 @@ public class Recruter {
 
     private String name;
     private List<JobPosition> jobPositionManaged;
-    private Set <String> specializedIndustries;
-    private Set <String> SpecializedRoles;
+    private Set<String> specializedIndustries;
+    private Set<String> specializedRoles;
 
-    public Recruter(String name, List<JobPosition> jobPositionManaged) {
+    public Recruter(String name) {
         this.name = name;
-        this.jobPositionManaged = jobPositionManaged;
+        jobPositionManaged = new ArrayList<>();
+        specializedRoles = new HashSet<>();
+        specializedIndustries = new HashSet<>();
     }
 
-    public Recruter(Set<String> specializedIndustries, Set<String> specializedRoles) {
-        this.specializedIndustries = specializedIndustries;
-        SpecializedRoles = specializedRoles;
+    public void assignJobPosition(JobPosition jobPosition) { // etiqueta
+        if (jobPosition != null) {   // se a posicao estiver vazia execute o conteudo abaixo
+            if (jobPositionManaged.contains(jobPosition)) {
+                jobPositionManaged.add(jobPosition);
+            } else {
+                System.out.println("No possible to add a new open position");
+            }
+        }
     }
-    public void assignJobPosition(JobPosition jobPosition){ // etiqueta
-        List<JobPosition> jobPositionList= new ArrayList<>();
+        public void reviewApplicant (Applicant applicant){
+            System.out.println(applicant.toString());
+            applicant.setStatus("Reviewed");
+        }
+            public boolean isSpecializedFor(JobPosition jobPosition) {
+                boolean hasSpecializedIndustry = specializedIndustries.contains(jobPosition.getIndustry());
+                boolean hasSpecializedRole = specializedRoles.contains(jobPosition.getRole());
+                return hasSpecializedIndustry && hasSpecializedRole;
+            }
 
-    }
 
-    public String getName() {
-        return name;
-    }
+            public String getName () {
+            return name;
+        }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+        public void setName (String name){
+            this.name = name;
+        }
 
-    public List<JobPosition> getJobPositionManaed() {
-        return jobPositionManaged;
-    }
+        public List<JobPosition> getJobPositionManaged () {
+            return jobPositionManaged;
+        }
 
-    public void setJobPositionManaed(List<JobPosition> jobPositionManaed) {
-        this.jobPositionManaged = jobPositionManaed;
+        public void setJobPositionManaged (List < JobPosition > jobPositionManaged) {
+            this.jobPositionManaged = jobPositionManaged;
+        }
+
+        public Set<String> getSpecializedIndustries () {
+            return specializedIndustries;
+        }
+
+        public void setSpecializedIndustries (Set < String > specializedIndustries) {
+            this.specializedIndustries = specializedIndustries;
+        }
+
+        public Set<String> getSpecializedRoles () {
+            return specializedRoles;
+        }
+
+        public void setSpecializedRoles (Set < String > specializedRoles) {
+            this.specializedRoles = specializedRoles;
+        }
     }
-}
